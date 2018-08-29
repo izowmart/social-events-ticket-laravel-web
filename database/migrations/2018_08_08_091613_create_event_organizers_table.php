bfaf5bf709653fd28fb9de55dcb1e15202cfaf3a
njorojohn <johnnjoroge40@gmail.com>
2018-08-29 21:12:04 +0300

    created event organizers table

--- database/migrations/2018_08_08_091613_create_event_organizers_table.php
+++ database/migrations/2018_08_08_091613_create_event_organizers_table.php
@@ -31,6 +31,6 @@ class CreateEventOrganizersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('event__organizers');
+        Schema::dropIfExists('event_organizers');
     }
 }
