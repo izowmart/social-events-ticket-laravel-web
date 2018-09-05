1141f3f22cae4ab2debf10828db01d4348d6ae81
njorojohn <johnnjoroge40@gmail.com>
2018-09-05 23:50:23 +0300

    created users seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'first_name' => 'John',
+            'last_name'  => 'Doe',
+            'username'   => 'johndoe',
+            'email'      => 'johnnjoroge40@gmail.com',
+            'year_of_birth' => '1990',
+            'gender'     => 1,
+            'image_url' => '5b903c5022bc21536179280.jpg',
+            'country_id' => 1,
+            'fcm_token' => 0,
+            'auto_follow_status' => 1,
+            'app_version_code' => '1.0.0',
+            'password'   => bcrypt('password'),
+            'created_at' =>now(),
+            'updated_at' =>now(),
+        ]);
+    }
+}
