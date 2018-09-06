968ce88e82435cf5cd21e140fbe3a2b57712003d
njorojohn <johnnjoroge40@gmail.com>
2018-09-06 20:09:56 +0300

    include seen for notifications

--- database/migrations/2018_08_08_091941_create_notifications_table.php
+++ database/migrations/2018_08_08_091941_create_notifications_table.php
@@ -22,6 +22,7 @@ class CreateNotificationsTable extends Migration
             $table->integer('type')->comment("1: Like,2: Share,3: Follow,4: Follow Request");
             $table->text('message');
             $table->integer("model_id")->nullable();
+            $table->boolean('seen')->default(false);
             $table->timestamps();
         });
     }
