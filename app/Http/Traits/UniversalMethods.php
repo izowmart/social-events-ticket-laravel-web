0094227f3a6582f6888fe945b11acf310d69d3d9
njorojohn <johnnjoroge40@gmail.com>
2018-09-06 14:04:57 +0300

    added UniversalMethods trait

---
+++ app/Http/Traits/UniversalMethods.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: phillip
+ * Date: 9/6/18
+ * Time: 1:24 PM
+ */
+
+namespace App\Http\Traits;
+
+
+trait UniversalMethods
+{
+    public static function getValidationErrorsAsString($errorArray)
+    {
+        $errorArrayTemp = [];
+        $error_strings = "";
+        foreach ($errorArray as $index => $item) {
+            $errStr = $item[0];
+            array_push($errorArrayTemp, $errStr);
+        }
+        if (!empty($errorArrayTemp)) {
+            $error_strings = implode('. ', $errorArrayTemp);
+        }
+
+        return $error_strings;
+    }
+
+}
