d9861f0176b8ad34cffa9d818b9f792c58a8fe6a
njorojohn <johnnjoroge40@gmail.com>
2018-10-06 00:04:00 +0300

    all API routes captured

--- routes/api.php
+++ routes/api.php
@@ -11,55 +11,59 @@
 |
 */
 
-$api = app('Dingo\Api\Routing\Router');
+$base_url = "";
+Route::group(['prefix' => 'auth'], function () use ($base_url) {
 
-$api->version('v1', function ($api) {
-    $base_url = "App\Http\Controllers\\";
-    $api->group(['prefix' => 'auth'], function ($api) use ($base_url) {
+    Route::group(['prefix' => 'user'], function () use ($base_url) {
+        //unauthenticated routes
+        Route::post('register', $base_url . 'Api\AuthController@register_user');
+        Route::post('login', $base_url . 'Api\AuthController@login_user');
+        Route::post('reset_password_email', $base_url . 'Api\AuthController@reset_password_user');
 
-        $api->group(['prefix' => 'user'], function ($api) use ($base_url) {
-            $api->get('users', $base_url . 'Api\AuthController@index');
-            $api->post('register', $base_url . 'Api\AuthController@register_user');
-            $api->post('login', $base_url . 'Api\AuthController@login_user');
-            $api->post('reset_password_email', $base_url . 'Api\AuthController@reset_password_user');
-            $api->post('update_auto_follow_status', $base_url . 'Api\AuthController@update_auto_follow_status');
-            $api->post('update_fcm_token', $base_url . 'Api\AuthController@update_fcm_token');
-        });
-
-        $api->group(['prefix' => 'scanner'], function ($api) use ($base_url) {
-            $api->get('scanners', $base_url . 'Api\ScannerAuthController@index');
-            $api->post('register', $base_url . 'Api\ScannerAuthController@register');
-            $api->post('login', $base_url . 'Api\ScannerAuthController@login');
-            $api->post('reset_password_email', $base_url . 'Api\ScannerAuthController@reset_password');
+        //authenticated
+        Route::group(['middleware' => 'auth:api'], function () use ($base_url) {
+            Route::get('users', $base_url . 'Api\AuthController@index');
+            Route::post('update_auto_follow_status', $base_url . 'Api\AuthController@update_auto_follow_status');
+            Route::post('update_fcm_token', $base_url . 'Api\AuthController@update_fcm_token');
         });
     });
 
-    $api->group(['prefix'=> 'user'], function ($api) use ($base_url) {
-        $api->get('adverts', $base_url . 'Api\AdvertController@index');
-        $api->post('adverts_view', $base_url . 'Api\AdvertController@advert_view');
+    Route::group(['prefix' => 'scanner'], function () use ($base_url) {
+        ///unauthenticated routes
+        Route::post('register', $base_url . 'Api\ScannerAuthController@register');
+        Route::post('login', $base_url . 'Api\ScannerAuthController@login');
+        Route::post('reset_password_email', $base_url . 'Api\ScannerAuthController@reset_password');
 
-        $api->get('countries', $base_url . 'Api\CountryController@index');
-        $api->get('events/{user_id}', $base_url . 'Api\EventController@index');
-        $api->get('notifications/{user_id}', $base_url . 'Api\NotificationController@index');
-        $api->post('notifications', $base_url . 'Api\NotificationController@markSeen');
-
-        $api->get('venues/{user_id}', $base_url . 'Api\VenueController@index');
-        $api->post('follow_venue', $base_url . 'Api\VenueController@follow_venue');
-
-        $api->get('posts/{user_id}', $base_url . 'Api\PostController@index');
-        $api->post('posts', $base_url . 'Api\PostController@store');
-        $api->post('delete_post', $base_url . 'Api\PostController@delete');
-        $api->post('like_post', $base_url . 'Api\PostController@like');
-        $api->post('report_abuse', $base_url . 'Api\PostController@report_abuse');
-        $api->get('{id}/relations', $base_url . 'Api\AuthController@user_relations');
-        $api->post('follow', $base_url . 'Api\AuthController@follow');
+        //authenticated ones
+        Route::group(['middleware' => 'auth:api'], function () use ($base_url) {
+            Route::get('scanners', $base_url . 'Api\ScannerAuthController@index');
+        });
     });
-
-    $api->group(['prefix'=> 'scanner'], function ($api) use ($base_url) {
-        $api->get('events/{scanner_id}',$base_url.'Api\EventController@scanner_events');
-
-    });
-
-    $api->get('payments/{user_id}/{event_id}', $base_url . 'Api\MulaPaymentController@initiate_payment');
 });
+
+Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function () use ($base_url) {
+    Route::get('adverts', $base_url . 'Api\AdvertController@index');
+    Route::post('adverts_view', $base_url . 'Api\AdvertController@advert_view');
+    Route::get('countries', $base_url . 'Api\CountryController@index');
+    Route::get('events/{user_id}', $base_url . 'Api\EventController@index');
+    Route::get('notifications/{user_id}', $base_url . 'Api\NotificationController@index');
+    Route::post('notifications', $base_url . 'Api\NotificationController@markSeen');
+    Route::get('venues/{user_id}', $base_url . 'Api\VenueController@index');
+    Route::post('follow_venue', $base_url . 'Api\VenueController@follow_venue');
+    Route::get('posts/{user_id}', $base_url . 'Api\PostController@index');
+    Route::post('posts', $base_url . 'Api\PostController@store');
+    Route::post('delete_post', $base_url . 'Api\PostController@delete');
+    Route::post('like_post', $base_url . 'Api\PostController@like');
+    Route::post('report_abuse', $base_url . 'Api\PostController@report_abuse');
+    Route::get('{id}/relations', $base_url . 'Api\AuthController@user_relations');
+    Route::post('follow', $base_url . 'Api\AuthController@follow');
+});
+
+Route::group(['prefix' => 'scanner', 'middleware' => 'api'], function () use ($base_url) {
+    Route::get('events/{scanner_id}', $base_url . 'Api\EventController@scanner_events');
+
+});
+
+Route::get('payments/{user_id}/{event_id}', $base_url . 'Api\MulaPaymentController@initiate_payment');
+
 
