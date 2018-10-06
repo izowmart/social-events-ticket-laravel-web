48d91fd91ebb8611588163113a903f865b12f696
njorojohn <johnnjoroge40@gmail.com>
2018-10-06 10:29:42 +0300

    custom password validation rule for mobile clients: user and scanner

---
+++ app/Helpers/ValidUserScannerPassword.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: phillip
+ * Date: 7/30/18
+ * Time: 5:21 PM
+ */
+
+namespace App\Helpers;
+
+
+use Illuminate\Contracts\Validation\Rule;
+
+class ValidUserScannerPassword implements Rule
+{
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string $attribute
+     * @param  mixed  $value
+     *
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        //check that the password contains at least 86characters, one uppercase and lowercase letter and a number
+        if (preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/m", $value)) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The :attribute must be at least 6 characters with a lowercase and uppercase letter and a number';
+    }
+}
