01247b5cf9a3b7cb78da40ea0dd3cf905bb0f7be
njorojohn <johnnjoroge40@gmail.com>
2018-11-07 12:33:13 +0300

    removed unneccessary seeders

---
+++ app/Transformers/AdvertViewTransformer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: phillip
+ * Date: 11/5/18
+ * Time: 2:47 PM
+ */
+
+namespace App\Transformers;
+
+
+use App\AdvertView;
+use League\Fractal\TransformerAbstract;
+
+class AdvertViewTransformer extends TransformerAbstract
+{
+
+    public function transform(AdvertView $advertView)
+    {
+        return [
+            'advert_id' => $advertView->advert_id,
+            'user_id'   => $advertView->user_id
+        ]
+    }
+
+}
