58a8e7ca18e8d0c4b9c79d8f9410f2dbdd378554
njorojohn <johnnjoroge40@gmail.com>
2018-11-13 15:40:30 +0300

    setup dompdf

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         LaravelFCM\FCMServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
     ],
 
@@ -210,6 +211,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'FCM'      => LaravelFCM\Facades\FCM::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 
