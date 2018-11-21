bce0461776aea1cf2be94ac86d2e53d5775203a0
Augustine Owuor Wafula <augustinetreezy@outlook.com>
2018-11-21 16:56:00 +0300

    added beauty mail pavkage. used beauty mail on email that nitifies scanners when they are added to event. included fika logo on email

---
+++ config/beautymail.php
@@ -0,0 +1,25 @@
+<?php
+ 
+return [
+ 
+    'colors' => [
+ 
+        'highlight' => '#f15b29',
+        'button'    => '#f15b29',
+ 
+    ],
+ 
+    'view' => [
+        'senderName'  => null,
+        'reminder'    => null,
+        'unsubscribe' => null,
+        'address'     => null,
+ 
+        'logo'        => [
+            'path'   => '%PUBLIC%/FikaIconWhite120x120.jpg',
+            'width'  => '120',
+            'height' => '100',
+        ]
+    ],
+ 
+];
