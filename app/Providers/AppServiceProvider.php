209c5cd7c50781485934921ee0114a1b759d7e43
njorojohn <johnnjoroge40@gmail.com>
2018-11-27 09:57:52 +0300

    stop forcing https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        If (env('APP_ENV') !== 'local') {
-            $this->app['request']->server->set('HTTPS', true);
-        }
+//        If (env('APP_ENV') !== 'local') {
+//            $this->app['request']->server->set('HTTPS', true);
+//        }
 
         Schema::defaultStringLength(191);
     }
