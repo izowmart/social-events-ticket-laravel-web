ba885f94392fc934c2ff360fa8263be5cfdc1379
njorojohn <johnnjoroge40@gmail.com>
2018-12-19 00:29:32 +0300

    now using black logo

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,5 +1,5 @@
 <!-- Navbar-->
-  <header class="app-header"><a class="app-header__logo" href="index.html">FIKA</a>
+<header class="app-header"><a class="app-header__logo" href="{{route('home')}}">FIKA Places</a>
     <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
     <!-- Navbar Right Menu-->
     <ul class="app-nav">
