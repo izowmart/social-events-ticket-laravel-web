1cfe94613fad8e598c1af67896d4f8079fd47a37
njorojohn <johnnjoroge40@gmail.com>
2019-01-10 12:10:08 +0300

    new home single event function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Event;
 use App\PaymentResponse;
+use App\TicketCategoryDetail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -81,6 +82,29 @@ class HomeController extends Controller
     public function showTermsAndConditions()
     {
         return view('terms-and-conditions');
+    }
+
+    public function single_event($slug)
+    {
+        //we fetch the particular event
+        $event = Event::select('events.id','events.name','events.description','events.location','events.latitude','events.longitude','ticket_category_details.price','ticket_category_details.no_of_tickets','events.type','events.status','events.slug','events.created_at','event_dates.start','event_dates.end','events.media_url','events.ticket_sale_end_date')
+            ->join('event_dates', 'event_dates.event_id', '=', 'events.id')
+            ->leftJoin('ticket_category_details', 'ticket_category_details.event_id', '=', 'events.id')
+            ->where('slug',$slug)
+            ->orderBy('id','desc')
+            ->first();
+        if($event==null){
+            return abort(404);
+        }
+        $ticket_categories = TicketCategoryDetail::select('ticket_category_details.price','ticket_category_details.category_id','ticket_category_details.no_of_tickets','ticket_categories.name','ticket_categories.slug')
+            ->where('ticket_category_details.event_id',$event->id)
+            ->join('ticket_categories', 'ticket_categories.id', '=', 'ticket_category_details.category_id')
+            ->get();
+        $data = array(
+            'event'=>$event,
+            'ticket_categories'=>$ticket_categories
+        );
+        return view('home.single_event')->with($data);
     }
 
 }
