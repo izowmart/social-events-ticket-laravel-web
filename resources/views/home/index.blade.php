38f9d7a91a52a9d8fc1e28aee119ebb814bfd586
njorojohn <johnnjoroge40@gmail.com>
2019-01-14 16:20:45 +0300

    made changes to the index page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -141,31 +141,31 @@
             <div class="row">
 
                 <div class="col-lg-4 wow zoomIn" data-wow-delay="0.2s">
-                    <div class="feature_home">
+                    <div class="feature_home" style="height: 380px;">
                         <i class="icon_set_1_icon-41"></i>
-                        <h3><span>200+</span> Tickets</h3>
+                        <h3><span>FIKA</span> Ticket Scanner</h3>
                         <p>
-                            Lorem ipsum dolor sit amet, vix erat audiam ei. Cum doctus civibus efficiantur in. Nec id tempor imperdiet deterruisset.
+                            Our Fika Ticket scanner allows users to scan tickets and instantaneously collect event data. Event organisers who use our Fika Ticket Scanner also benefit by having their event advertised on both our website and on our Fika PLACES App. Tickets for you event can also be bought from these two platforms.
                         </p>
                     </div>
                 </div>
 
                 <div class="col-lg-4 wow zoomIn" data-wow-delay="0.4s">
-                    <div class="feature_home">
+                    <div class="feature_home" style="height: 380px;">
                         <i class="icon_set_1_icon-30"></i>
-                        <h3><span>1000+</span> Customers</h3>
+                        <h3><span>Customers</span> </h3>
                         <p>
-                            Lorem ipsum dolor sit amet, vix erat audiam ei. Cum doctus civibus efficiantur in. Nec id tempor imperdiet deterruisset.
+                            Currently our customers are mainly from Kenya but we aim to expand across various countries allowing our expanding community to enjoy the vast experiences that the world has to offer.
                         </p>
                     </div>
                 </div>
 
                 <div class="col-lg-4 wow zoomIn" data-wow-delay="0.6s">
-                    <div class="feature_home">
+                    <div class="feature_home" style="height: 380px;">
                         <i class="icon_set_1_icon-57"></i>
-                        <h3><span>24h </span> Support</h3>
+                        <h3><span>Support </span> </h3>
                         <p>
-                            Lorem ipsum dolor sit amet, vix erat audiam ei. Cum doctus civibus efficiantur in. Nec id tempor imperdiet deterruisset.
+                            For any enquiries you can contact us via <a href="mailto:info@fikaplaces.com">info@fikaplaces.com</a>. Kindly ensure that the subject is relevant to the enquiry.
                         </p>
                     </div>
                 </div>
@@ -186,14 +186,12 @@
                 </div>
                 <div class="col-md-6">
                     <h3><span>Get started</span> with Fika Places App</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, vix erat audiam ei. Cum doctus civibus efficiantur in. Nec id tempor imperdiet deterruisset.
-                    </p>
                     <ul class="list_order">
-                        <li><span>1</span>Lorem ipsum dolor sit amet, vix erat audiam ei.</li>
-                        <li><span>2</span>Lorem ipsum dolor sit amet, vix erat audiam ei.</li>
-                        <li><span>3</span>Lorem ipsum dolor sit amet, vix erat audiam ei.</li>
-                        <li><span>4</span>Lorem ipsum dolor sit amet, vix erat audiam ei.</li>
+                        <li><span>1</span>Download Fika Places from Google Play store for FREE.</li>
+                        <li><span>2</span>Create a user profile and sign in.</li>
+                        <li><span>3</span>Verify your email address.</li>
+                        <li><span>4</span>Start looking for Venues and Events to Enjoy!!</li>
+                        <li><span>5</span>Recommend Fika Places to your friends</li>
 
                     </ul>
                     <a href="https://play.google.com/store/apps/details?id=com.fika.places" target="_blank"><img src="{{url('images/home/playstore.png')}}" alt="play store"></a>
