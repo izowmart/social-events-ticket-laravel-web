e84248e0d6bbcd3cad8582a9c6b4a2acfd1835f7
njorojohn <johnnjoroge40@gmail.com>
2019-01-15 11:51:55 +0300

    changes to about us page

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -19,25 +19,16 @@
     <div class="container margin_60">
 
         <div class="post">
-            <p> Fika Places is a Kenyan based company that provides people with information regarding the
-            entertainment sector in Kenya. More specifically we identify and highlight the best places to eat and the
-            best places to party  </p>
-            <p> You can download our App <a href="http://play.google.com/store/apps/details?id=com.fika.places">here</a> and begin to discover new establishments for your entertainment.
-            Our app allows you to take and share pictures/videos in different venues. The content will be seen by
-            others and used almost as a review, allowing them to understand what that particular venue looks/feels
-            like before they visit.</p>
-            <p>Fika Places also displays venue contact details and GPS location, allowing users to contact the venue to
-            make bookings or use its location for directions.</p>
-            <p>All content on the Fika Places mobile application is user generated and therefore images and videos that
-            you see are accurate representations of the venues you visit.</p>
-            <p>Sharing the app with friends will enable you to see a variety of venues, allowing you to experience more.</p>
+            <p>Fika Places is a Kenyan based company that provides it's customers with information regarding nightlife, leisure and entertainment in Kenya. To be specific, we highlight the best places to spend your free time.
+            </p>
+            <p>
+                You can download our App <a href="http://play.google.com/store/apps/details?id=com.fika.places">here</a> and begin to discover new venues that suit your style of entertainment. Our App (Fika Places) allows you to record and share pictures/videos from different venues, giving you the chance to show your friends and others the authentic feel of a venue.
+            </p>
+            <p>Fika Places provides users with venue contacts, as well as GPS location. Making it easier to make reservations and/or get directions. </p>
+            <p>In addition to all of this, our customers can also buy tickets for various events through the Fika Places App. We provide the option of buying tickets from our website as well, but customers who buy using the App will qualify for gifts and offers in specific events.</p>
+            <p>See Share and Love your City.</p>
 
-            <p>In addition to all those functions, Fika Places also has tickets for various events, which users can buy.
-            Tickets can be bought directly from the app, or from our website. However, App ticket holders will have
-            more benefits in some events.</p>
-
-            <p>See Share and Love you City</p>
-            <p>Download Fika Places now.</p>
+            <p><a href="http://play.google.com/store/apps/details?id=com.fika.places">Download Fika Places now.</a></p>
         </div>
     </div>
     <!-- End container -->
